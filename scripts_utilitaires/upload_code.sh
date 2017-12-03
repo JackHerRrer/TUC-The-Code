8a9d35a8ae6289a22906576b9b0ed809c4f708f3
Jack HerRrer <maxime.perrigault@protonmail.com>
2017-12-04 00:35:54 +0100

    Début de l'ajout des calculs de combos

--- scripts_utilitaires/upload_code.sh
+++ scripts_utilitaires/upload_code.sh
@@ -20,17 +20,29 @@ IA_PATH=$COMMAND_PATH/../IA/
 IA_NAME_1=GlobalVar
 IA_FILE_NAME_1="${IA_PATH}/${IA_NAME_1}"
 
-IA_NAME_2=Utilitaires
+IA_NAME_2=util_misc
 IA_FILE_NAME_2="${IA_PATH}/${IA_NAME_2}"
 
-IA_NAME_3=Strategy
+IA_NAME_3=util_can_use
 IA_FILE_NAME_3="${IA_PATH}/${IA_NAME_3}"
 
-IA_NAME_4=FonctionEtat
+IA_NAME_4=util_score
 IA_FILE_NAME_4="${IA_PATH}/${IA_NAME_4}"
 
-IA_NAME_5=IA
+IA_NAME_5=util_filters
 IA_FILE_NAME_5="${IA_PATH}/${IA_NAME_5}"
+
+IA_NAME_6=util_combos
+IA_FILE_NAME_6="${IA_PATH}/${IA_NAME_6}"
+
+IA_NAME_7=Strategy
+IA_FILE_NAME_7="${IA_PATH}/${IA_NAME_7}"
+
+IA_NAME_8=FonctionEtat
+IA_FILE_NAME_8="${IA_PATH}/${IA_NAME_8}"
+
+IA_NAME_9=IA
+IA_FILE_NAME_9="${IA_PATH}/${IA_NAME_9}"
 
 
 connect
@@ -40,5 +52,9 @@ upload_code ${IA_NAME_2} ${IA_FILE_NAME_2}
 upload_code ${IA_NAME_3} ${IA_FILE_NAME_3}
 upload_code ${IA_NAME_4} ${IA_FILE_NAME_4}
 upload_code ${IA_NAME_5} ${IA_FILE_NAME_5}
+upload_code ${IA_NAME_6} ${IA_FILE_NAME_6}
+upload_code ${IA_NAME_7} ${IA_FILE_NAME_7}
+upload_code ${IA_NAME_8} ${IA_FILE_NAME_8}
+upload_code ${IA_NAME_9} ${IA_FILE_NAME_9}
 
 disconnect
